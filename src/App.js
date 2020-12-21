d9337d65f685dac4a50a9db07e0c2aeeba5f704a
hzhu <hz@henryzhu.me>
2020-12-21 12:29:18 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "@zendeskgarden/react-theming";
+import {
+  Dropdown,
+  Field,
+  Menu,
+  Item,
+  Select,
+  Label,
+} from "@zendeskgarden/react-dropdowns";
 
-function App() {
+const items = [
+  { label: "Fern", value: "item-1" },
+  { label: "Snake plant", value: "item-2" },
+  { label: "Rubber tree", value: "item-3" },
+];
+
+export default function App() {
+  const [selectedItem, setSelectedItem] = React.useState(items[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <Dropdown
+        selectedItem={selectedItem}
+        onSelect={setSelectedItem}
+        downshiftProps={{ itemToString: (item) => item && item.label }}
+      >
+        <Field>
+          <Label>Houseplant</Label>
+          <Select>{selectedItem.label}</Select>
+        </Field>
+        <Menu>
+          {items.map((option) => (
+            <Item key={option.value} value={option}>
+              {option.label}
+            </Item>
+          ))}
+        </Menu>
+      </Dropdown>
+    </ThemeProvider>
   );
 }
-
-export default App;
